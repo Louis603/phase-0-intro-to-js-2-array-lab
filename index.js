edb30fa0ed9b03b670f58d8ca83139acb7c15119
Louis Y <louisy603@hotmail.com>
2022-04-10 19:07:33 -0400

    commit tree 5757c456fdfffe847b2b578a24a8a6e20ee20a82

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+function destructivelyAppendCat () {
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat () {
+    cats.unshift('Bob')
+}
+
+function destructivelyRemoveLastCat() {
+    cats.splice(2)
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.splice(0,1)
+}
+
+function appendCat() {
+    return [...cats, 'Broom']
+};
+
+function prependCat() {
+    return ['Arnold', ...cats]
+}
+
+function removeLastCat() {
+    return cats.slice(0,2)
+}
+
+function removeFirstCat() {
+    return cats.slice(1)
+}
